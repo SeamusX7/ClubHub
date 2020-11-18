36c6747e34cb43c7eeeb0b79d197e429c792d233
Cheuk Wei <cheukwei777@gmail.com>
2020-11-18 22:22:13 +0000

    Created messages stack navigation #5

--- src/routes/SessionStack.js
+++ src/routes/SessionStack.js
@@ -7,18 +7,22 @@ import PreviousTrainingSessionsScreen from '../screens/session/PreviousTrainingS
 import PreviousGymSessionsScreen from '../screens/session/PreviousGymSessions';
 import ViewUpcomingMatchSessionScreen from '../screens/session/ViewUpcomingMatchSession';
 import ViewPreviousMatchSessionScreen from '../screens/session/ViewPreviousMatchSession';
+import MessageScreen from '../screens/team/Message'
+import ViewMessageScreen from '../screens/team/ViewMessage'
 
 const SessionStack = createStackNavigator();
 
 export default function SessionStackScreen() {
   return (
-    <SessionStack.Navigator screenOptions={{ headerTitleStyle: { color: '#333', fontFamily: 'montserrat-semibold' }, headerBackTitleVisible: false  }}>
+    <SessionStack.Navigator screenOptions={{ headerTitleStyle: { color: '#333', fontFamily: 'montserrat-semibold' }, headerBackTitleVisible: false }}>
       <SessionStack.Screen name="Session" component={SessionScreen} options={{ title: 'Sessions', headerLeft: null }} ></SessionStack.Screen>
       <SessionStack.Screen name="PreviousMatchSessions" component={PreviousMatchSessionsScreen} options={{ title: 'Previous Match Sessions' }} ></SessionStack.Screen>
       <SessionStack.Screen name="PreviousTrainingSessions" component={PreviousTrainingSessionsScreen} options={{ title: 'Previous Training Sessions' }} ></SessionStack.Screen>
       <SessionStack.Screen name="PreviousGymSessions" component={PreviousGymSessionsScreen} options={{ title: 'Previous Gym Sessions' }} ></SessionStack.Screen>
       <SessionStack.Screen name="ViewUpcomingMatchSession" component={ViewUpcomingMatchSessionScreen} options={{ title: 'View Upcoming Match Session' }} ></SessionStack.Screen>
       <SessionStack.Screen name="ViewPreviousMatchSession" component={ViewPreviousMatchSessionScreen} options={{ title: 'View Previous Match Session' }} ></SessionStack.Screen>
+      <SessionStack.Screen name="Message" component={MessageScreen} options={{ title: 'Message' }} ></SessionStack.Screen>
+      <SessionStack.Screen name="ViewMessage" component={ViewMessageScreen} options={{ title: 'View Message' }} ></SessionStack.Screen>
     </SessionStack.Navigator>
   )
 }
--- src/screens/team/Team.js
+++ src/screens/team/Team.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { StyleSheet, View, Button } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function TeamScreen({ navigation }) {
   return (
+    React.useLayoutEffect(() => {
+      navigation.setOptions({
+        headerRight: () => (
+          <View style={styles.icon} >
+            <Ionicons onPress={() => navigation.navigate('Message')} name="md-text" size={24} color={'#B7B7B7'} />
+          </View>
+        ),
+      });
+    }, [navigation]),
+
     <View style={styles.container}>
-      <Button 
+      <Button
         title="Player"
         onPress={() => navigation.navigate('Player')} />
     </View>
@@ -16,4 +27,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#f0f2f7',
   },
+  icon: {
+    marginRight: 20,
+  }
 });
