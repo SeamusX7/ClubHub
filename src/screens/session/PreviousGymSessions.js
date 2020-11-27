438f8f1de84d4599845bec53052e7da1af81fb71
SeamusX7 <s.cumminsjnr@gmail.com>
2020-11-27 14:50:20 +0000

    Added search section to previous sessions screens #70

--- src/screens/session/PreviousGymSessions.js
+++ src/screens/session/PreviousGymSessions.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import { StyleSheet, View, Button } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+
+// Local File Imports
+import Search from '../../components/Search';
+import SearchButton from '../../components/SearchButton';
+import global_styles from '../../assets/styles/GlobalStyle';
 
 export default function PreviousGymSessionsScreen({ navigation }) {
   return (
     <View style={styles.container}>
+      <View style={global_styles.searchSection}>
+        <Search />
+        <SearchButton>
+          <MaterialIcons
+            name='filter-list'
+            size={24}
+            color="#b7b7b7" />
+        </SearchButton>
+      </View>
+
       <Button
         title="View gym session"
         onPress={() => navigation.navigate('ViewPreviousGymSession')} />
@@ -15,6 +31,6 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#f0f2f7',
     flex: 1,
-    margin: 20,
+    padding: 20,
   }
 });
