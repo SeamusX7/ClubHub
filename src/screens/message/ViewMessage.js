e9d507d21be07feb3b5036c7ecdbcb75f31f1f56
Brandon Kennedy <brandn18@gmail.com>
2021-01-06 17:06:51 +0000

    #105 Initial Messaging System

--- src/screens/message/ViewMessage.js
+++ src/screens/message/ViewMessage.js
@@ -1,56 +1,65 @@
 import React from 'react';
 import { StyleSheet, ScrollView, View, Text, SafeAreaView, KeyboardAvoidingView } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { Platform } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 // Local File Imports
 import message_styles from '../../assets/styles/MessageStyle';
 import SendMessageInput from '../../components/SendMessage';
-
+import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 export default function ViewMessageScreen() {
+
   return (
+      <WebView 
+        source={ Platform.OS === 'ios' ? require('../../assets/talkjs.html') : { uri: "file:///talkjs.html" }}
+        style={{flex: 1}} 
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+      />
+    // <SafeAreaView style={styles.container}>
 
-    <SafeAreaView style={styles.container}>
+    //   <KeyboardAwareScrollView>
+    //     <View style={message_styles.textMessageBoxOne} >
+    //       <Text style={message_styles.textMessageOne}>Lorem ipsum dolor sit amet.</Text>
+    //       <Text style={message_styles.textTimeOne}>19:45</Text>
+    //     </View>
 
-      <KeyboardAwareScrollView>
-        <View style={message_styles.textMessageBoxOne} >
-          <Text style={message_styles.textMessageOne}>Lorem ipsum dolor sit amet.</Text>
-          <Text style={message_styles.textTimeOne}>19:45</Text>
-        </View>
+    //     <View style={message_styles.textMessageBoxTwo} >
+    //       <Text style={message_styles.textMessageTwo}>Lorem ipsum dolor sit amet.</Text>
+    //     </View>
 
-        <View style={message_styles.textMessageBoxTwo} >
-          <Text style={message_styles.textMessageTwo}>Lorem ipsum dolor sit amet.</Text>
-        </View>
+    //     <View style={message_styles.profile}>
+    //       <View style={message_styles.textMessageBoxThree} >
+    //         <Text style={message_styles.textMessageThree}>Lorem ipsum dolor.</Text>
+    //         <Text style={message_styles.textTimeThree}>19:45</Text>
+    //       </View>
+    //     </View>
 
-        <View style={message_styles.profile}>
-          <View style={message_styles.textMessageBoxThree} >
-            <Text style={message_styles.textMessageThree}>Lorem ipsum dolor.</Text>
-            <Text style={message_styles.textTimeThree}>19:45</Text>
-          </View>
-        </View>
+    //     <View style={{ flexDirection: 'row' }}>
+    //       <View style={message_styles.line} />
+    //       <Text style={message_styles.newMessages}>New Messages</Text>
+    //       <View style={message_styles.line} />
+    //     </View>
 
-        <View style={{ flexDirection: 'row' }}>
-          <View style={message_styles.line} />
-          <Text style={message_styles.newMessages}>New Messages</Text>
-          <View style={message_styles.line} />
-        </View>
+    //     <View style={message_styles.profileTwo}>
+    //       <View style={message_styles.textMessageBoxThree} >
+    //         <Text style={message_styles.textMessageThree}>Lorem ipsum dolor.</Text>
+    //         <Text style={message_styles.textTimeThree}>19:48</Text>
+    //       </View>
+    //     </View>
+    //   </KeyboardAwareScrollView>
 
-        <View style={message_styles.profileTwo}>
-          <View style={message_styles.textMessageBoxThree} >
-            <Text style={message_styles.textMessageThree}>Lorem ipsum dolor.</Text>
-            <Text style={message_styles.textTimeThree}>19:48</Text>
-          </View>
-        </View>
-      </KeyboardAwareScrollView>
+    //   <KeyboardAvoidingView
+    //     behavior="padding"
+    //   >
+    //     <View style={{ flex: 1 }}>
+    //       <SendMessageInput />
+    //     </View>
+    //   </KeyboardAvoidingView>
 
-      <KeyboardAvoidingView
-        behavior="padding"
-      >
-        <View style={{ flex: 1 }}>
-          <SendMessageInput />
-        </View>
-      </KeyboardAvoidingView>
-
-    </SafeAreaView>
+    // </SafeAreaView>
 
   )
 }
