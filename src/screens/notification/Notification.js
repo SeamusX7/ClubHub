4f1c165961c33ac561ba032a6af0144bf507d6db
Brandon Kennedy <brandn18@gmail.com>
2021-02-08 20:28:29 +0000

    Large notification changes #107

--- src/screens/notification/Notification.js
+++ src/screens/notification/Notification.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text} from 'react-native';
-import { Ionicons, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+import { StyleSheet, View, Text,} from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 // Local File Imports
 import Card from '../../components/NotificationCard';
+import Card2 from '../../components/NotificationCardLarge';
 import notification_styles from '../../assets/styles/NotificationStyle';
 import MoreButton from '../../components/MoreButton';
+import AcceptButton from '../../components/AcceptButton';
+import DeclineButton from '../../components/DeclineButton';
 
 export default function NotificationScreen({ navigation }) {
+
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
   return (
     <View style={styles.container}>
        <Card onPress={() => navigation.navigate('Message')}>
@@ -85,6 +92,43 @@ export default function NotificationScreen({ navigation }) {
           </View>
         </View>
       </Card>
+
+      <Card2 style={notification_styles.card2}>
+        <View style={notification_styles.container}>
+          <View style={notification_styles.circle}>
+            <MaterialCommunityIcons
+              name='soccer'
+              size={20}
+              color='#5386e4'
+              style={notification_styles.icon} />
+          </View>
+          <View style={notification_styles.textView}>
+            <Text style={notification_styles.textOne}>Training Session</Text>
+            <Text style={notification_styles.textSpace}>Are you able to attend training tomorrow at 6pm?</Text>
+            <View style={{ flexDirection:"row" }}>
+          <View>
+            <AcceptButton
+              text="Accept"
+              onPress={() => {
+              alert('You have accepted training!');
+            }}
+            />
+          </View>
+          <View>
+            <DeclineButton
+              text="Decline"
+              onPress={() => {
+              alert('You have declined training!');
+            }}
+            />
+          </View>
+          </View>
+          </View>
+          <View style={notification_styles.more}>
+            <MoreButton onPress={() => console.log('Tap')} />
+          </View>
+        </View>
+      </Card2>
     </View>
   )
 }
