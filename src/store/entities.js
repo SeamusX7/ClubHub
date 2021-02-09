fdcaa541fdfe9c5c1a1b338590f7cad009eeff88
Joel Dyas Kelly <d00203711@student.dkit.ie>
2021-02-09 10:34:39 +0000

    initial commit for sessions page #154

--- src/store/entities.js
+++ src/store/entities.js
@@ -1,14 +1,18 @@
 import { combineReducers } from 'redux';
 import userReducer from './user';
 import teamsReducer from './teams';
+import sessionsReducer from './sessions';
 import activeTeamReducer from './activeTeam';
+import activeSessionReducer from './activeSession';
 // import projectsReducer from './projects';
 // import usersReducer from './users';
 
 export default combineReducers({
 user : userReducer,
 teams : teamsReducer,
+sessions : sessionsReducer,
 activeTeam : activeTeamReducer,
+activeSession : activeSessionReducer
 // projects : projectsReducer,
 // users : usersReducer
 })
