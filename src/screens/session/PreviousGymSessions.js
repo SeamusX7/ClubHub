57dfd42ed51e1f70f5f3e3258030782c24be7872
Joel Dyas Kelly <d00203711@student.dkit.ie>
2021-02-22 20:25:23 +0000

    Created and displayed the previous session arrays for all session types #165

--- src/screens/session/PreviousGymSessions.js
+++ src/screens/session/PreviousGymSessions.js
@@ -1,17 +1,105 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
+import React, { useState, useEffect } from 'react';
+import { Ionicons, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+import { StyleSheet, Text, View, Button, TouchableOpacity, TextInput, FlatList, ScrollView, ActivityIndicator } from 'react-native';
+import { firebase } from '../../firebase/config';
+import { SafeAreaView, Modal } from 'react-native';
 
 // Local File Imports
+import global_styles from '../../assets/styles/GlobalStyle';
+import large_card_style from '../../assets/styles/LargeCardStyle';
+import Card from '../../components/Card';
+import card_styles from '../../assets/styles/CardStyle';
+import MoreButton from '../../components/MoreButton';
+import FlatButton from '../../components/CreateButton';
+import modal_styles from '../../assets/styles/ModalStyle';
+import NewSessionModal from './NewSessionModal';
 import Search from '../../components/Search';
 import SearchButton from '../../components/SearchButton';
-import global_styles from '../../assets/styles/GlobalStyle';
-import card_styles from '../../assets/styles/CardStyle';
-import Card from '../../components/Card';
-import MoreButton from '../../components/MoreButton';
 
-export default function PreviousGymSessionsScreen({ navigation }) {
+//redux
+import { useDispatch , useSelector } from 'react-redux';
+import { teamsAdded, getTeams } from '../../store/teams';
+import { activeTeamAdded, getActiveTeamKey } from '../../store/activeTeam';
+import { sessionsAdded, getSessions } from '../../store/sessions';
+import { activeSessionAdded, getactiveSessionKey, activeSessionRemove } from '../../store/activeSession';
+import { getUserType } from '../../store/user';
+
+
+export default function PreviousMatchSessionsScreen({navigation}) {
+ 
+  const teamID = useSelector(getActiveTeamKey);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [sessionIdKey, setSessionIdKey] = useState('session id');
+  const openModalButton = <FlatButton onPress={() => setModalOpen(true)}/>
+  const uType = useSelector(getUserType);
+
+
+  const activeModal = (fact, key) => {
+    setSessionIdKey(key.item.key);
+    closeModal(fact);
+  }
+
+  const closeModal = (fact) => {
+    setModalOpen(false);
+  }
+
+  const dispatch = useDispatch();
+  const sessionsArray = useSelector(getSessions);
+  const db = firebase.firestore();
+  const [sessions, setSessions] = useState([]); // Initial empty array of sessions
+  const [activeSession, setActiveSession] = useState([]);
+
+  
+  console.log('sessionsArray ===> : ', sessionsArray);
+
+  useEffect(() => {
+
+    db.collection('sessions')
+    .where('teamId', '==', teamID)
+    .where('sessionType', '==', "gym")
+    .get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        sessions.push({
+          ...doc.data(),
+          key: doc.id,
+        });
+      });
+
+      setSessions(sessions);
+      dispatch(sessionsAdded(sessions));
+
+    });
+
+   
+  }, [sessions]);
+
+  sessionSelected = item =>
+  {
+    setActiveSession(item);
+    dispatch(activeSessionRemove());
+    dispatch(activeSessionAdded(item));
+
+     console.log('session selected item ==> : ', item);
+
+     let sessionType = item.item.sessionType;
+     if(sessionType === 'match')
+     {
+      //navigation.navigate('ViewUpcomingMatchSession')
+     }
+  }
+
   return (
+    React.useLayoutEffect(() => {
+      navigation.setOptions({
+        headerRight: () => (
+          <View style={styles.icon} >
+            <MaterialCommunityIcons onPress={() => navigation.navigate('Message')} name="message-text-outline" size={24} color={'#b7b7b7'} />
+          </View>
+        ),
+      });
+    }, [navigation]),
+
     <View style={styles.container}>
       <View style={global_styles.searchSection}>
         <Search />
@@ -22,29 +110,47 @@ export default function PreviousGymSessionsScreen({ navigation }) {
             color="#b7b7b7" />
         </SearchButton>
       </View>
-
-      <View style={{ marginTop: 30 }}>
-        <Card onPress={() => navigation.navigate('ViewPreviousGymSession')}>
-          <View style={card_styles.container}>
-            <View style={card_styles.textViewNoCircle}>
-              <Text style={card_styles.textOne}>Compound lifts</Text>
-              <Text style={card_styles.textTwo}>3rd December 2019</Text>
+      
+      <Text style={{...global_styles.title, marginBottom: 4, marginTop: 30 }}>Previous matches</Text>
+      <FlatList
+        data={sessions}
+        renderItem={({ item }) => (
+          <Card
+            onPress={() => this.sessionSelected({item})}>
+            <View style={card_styles.container}>
+              <View style={card_styles.circle} >
+                {item.sessionType=="match" ? <MaterialCommunityIcons name='trophy-outline' size={20} color='#5386e4' style={card_styles.icon} />
+                  : item.sessionType=="training" ? <Ionicons name='md-football' size={20} color='#5386e4' style={{...card_styles.icon, marginTop:11, marginLeft:2}} />
+                  : <MaterialCommunityIcons name='dumbbell' size={20} color='#5386e4' style={card_styles.icon} /> }
+              </View>
+              <View style={card_styles.textView} >
+                {item.sessionType=="match" ? <Text style={card_styles.textOne}>vs. {item.opposition}</Text> : <Text style={card_styles.textOne}>{item.title}</Text>}
+                <Text style={card_styles.textTwo}>{item.timeStamp.toDate().toDateString()} | {item.timeStamp.toDate().toLocaleTimeString('en-US')}</Text>
+              </View>
+              <View style={card_styles.more} >
+                <MoreButton onPress={() => activeModal(true, { item })} />
+              </View>
             </View>
-            <View style={card_styles.more}>
-              <MoreButton />
-            </View>
-          </View>
-        </Card>
-      </View>
+          </Card>
+        )}
+      />
 
     </View>
   )
+  
 }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#f0f2f7',
     flex: 1,
-    padding: 20,
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+  icon: {
+    marginRight: 20,
   }
 });
