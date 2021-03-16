bef424b762e0ac150f61418d20b53cbdb0106a9f
Cheuk Wei <cheukwei777@gmail.com>
2021-03-16 13:04:48 +0000

    Minor message system changes & added dependency packages #243

--- src/screens/message/Message.js
+++ src/screens/message/Message.js
@@ -93,72 +93,77 @@ export default function App() {
   const [messages, setMessages] = useState([])
 
   useEffect(() => {
-      readUser()
-      const unsubscribe = chatsRef.onSnapshot((querySnapshot) => {
-          const messagesFirestore = querySnapshot
-              .docChanges()
-              .filter(({ type }) => type === 'added')
-              .map(({ doc }) => {
-                  const message = doc.data()
-                  //createdAt is firebase.firestore.Timestamp instance
-                  //https://firebase.google.com/docs/reference/js/firebase.firestore.Timestamp
-                  return { ...message, createdAt: message.createdAt.toDate() }
-              })
-              .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
-          appendMessages(messagesFirestore)
-      })
-      return () => unsubscribe()
+    readUser()
+    const unsubscribe = chatsRef.onSnapshot((querySnapshot) => {
+      const messagesFirestore = querySnapshot
+        .docChanges()
+        .filter(({ type }) => type === 'added')
+        .map(({ doc }) => {
+          const message = doc.data()
+          //createdAt is firebase.firestore.Timestamp instance
+          //https://firebase.google.com/docs/reference/js/firebase.firestore.Timestamp
+          return { ...message, createdAt: message.createdAt.toDate() }
+        })
+        .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      appendMessages(messagesFirestore)
+    })
+    return () => unsubscribe()
   }, [])
 
   const appendMessages = useCallback(
-      (messages) => {
-          setMessages((previousMessages) => GiftedChat.append(previousMessages, messages))
-      },
-      [messages]
+    (messages) => {
+      setMessages((previousMessages) => GiftedChat.append(previousMessages, messages))
+    },
+    [messages]
   )
 
   async function readUser() {
-      const user = await AsyncStorage.getItem('user')
-      if (user) {
-          setUser(JSON.parse(user))
-      }
+    const user = await AsyncStorage.getItem('user')
+    if (user) {
+      setUser(JSON.parse(user))
+    }
   }
   async function handlePress() {
-      const _id = Math.random().toString(36).substring(7)
-      const user = { _id, name }
-      await AsyncStorage.setItem('user', JSON.stringify(user))
-      setUser(user)
+    const _id = Math.random().toString(36).substring(7)
+    const user = { _id, name }
+    await AsyncStorage.setItem('user', JSON.stringify(user))
+    setUser(user)
   }
   async function handleSend(messages) {
-      const writes = messages.map((m) => chatsRef.add(m))
-      await Promise.all(writes)
+    const writes = messages.map((m) => chatsRef.add(m))
+    await Promise.all(writes)
   }
 
   if (!user) {
-      return (
-          <View style={styles.container}>
-              <TextInput style={styles.input} placeholder="Enter your name" value={name} onChangeText={setName} />
-              <Button onPress={handlePress} title="Enter the chat" />
-          </View>
-      )
+    return (
+      <View style={styles.container}>
+        <TextInput style={styles.input} placeholder="Enter your name" value={name} onChangeText={setName} />
+        <Button onPress={handlePress} title="Enter the chat" />
+      </View>
+    )
   }
-  return <GiftedChat messages={messages} user={user} onSend={handleSend} />
+  return <GiftedChat
+    messages={messages}
+    user={user} onSend={handleSend}
+    alwaysShowSend={true}
+    renderUsernameOnMessage={true}
+  />
 }
 
 const styles = StyleSheet.create({
   container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding: 30,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 30,
   },
   input: {
-      height: 50,
-      width: '100%',
-      borderWidth: 1,
-      padding: 15,
-      marginBottom: 20,
-      borderColor: 'gray',
+    height: 50,
+    width: '100%',
+    borderWidth: 1,
+    padding: 15,
+    marginBottom: 20,
+    borderColor: 'gray',
   },
 })
